a7267caec3bbfbcd69c1d6454860843835d3535c
SamsonOlatunde <102227754+SamsonOlatunde@users.noreply.github.com>
2022-07-09 04:21:22 -0700

    Add files via upload

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'marketplace.urls'
@@ -111,6 +113,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT=[os.path.join(BASE_DIR, 'staticfile')]
 
 STATIC_URL = 'static/'
 
